acb52adde60c7b2403170d503081a11d6e723780
SimonThornblad <thornblad.simon@gmail.com>
2021-09-08 13:55:45 +0200

    excel

---
+++ src/mainPack/Mocks/WriteFile.java
@@ -0,0 +1,22 @@
+package mainPack.Mocks;
+
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+
+public class WriteFile {
+
+
+
+    public void writeAction(String action) throws FileNotFoundException {
+
+        String fileName = "inputData.csv";
+
+        FileOutputStream fileOutput = new FileOutputStream(fileName, false);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fileOutput));
+
+
+
+    }
+}
